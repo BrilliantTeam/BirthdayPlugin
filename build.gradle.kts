0fe16a93cc53ea69c4f8bedaebaf4e8e7ba97cd2
菘菘 <rrt467778@gmail.com>
2022-08-17 15:06:01 +0800

    feat: remove unused library

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,8 +32,8 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${project.property("kotlin_version")}")?.let { bundle(it) }
-    implementation("org.jetbrains.kotlin:kotlin-reflect:${project.property("kotlin_version")}")?.let { bundle(it) }
-//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")?.let { bundle(it) }
+//  implementation("org.jetbrains.kotlin:kotlin-reflect:${project.property("kotlin_version")}")?.let { bundle(it) }
+//  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")?.let { bundle(it) }
 
     compileOnly("org.spigotmc:spigot-api:${project.property("spigot_version").toString()}")
     compileOnly("me.clip:placeholderapi:2.11.2")
